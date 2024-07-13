35119c684c9b09cd61fe5ec1ce2f09e14bffee1d
SHWETA-KALE <99083461+SHWETA-KALE@users.noreply.github.com>
2024-07-13 22:02:43 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeModified
+{
+    internal class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter the number: ");
+                int input = int.Parse(Console.ReadLine());
+
+                bool isPrime = true;
+
+                for (int div = 2; div <= Math.Sqrt(input); div++)
+                {
+                    if (input % div == 0)
+                    {
+                        isPrime = false;
+                        break; 
+                    }
+                }
+
+                if (input <= 1)
+                {
+                    Console.WriteLine(input + " is not a Prime number"); // 1 and negative numbers are not prime
+                }
+                else if (isPrime)
+                {
+                    Console.WriteLine(input + " is a Prime Number");
+                }
+                else
+                {
+                    Console.WriteLine(input + " is not a Prime number");
+                }
+            
+        }
+
+    }
+}
